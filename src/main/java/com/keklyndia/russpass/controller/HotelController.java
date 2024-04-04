b9062e095fcb35745313778c428fc9f880f946f4
M0nenvoy <s.maluhin@astondevs.ru>
2024-04-04 17:38:21 +0400

    Изменения

--- src/main/java/com/keklyndia/russpass/controller/HotelController.java
+++ src/main/java/com/keklyndia/russpass/controller/HotelController.java
@@ -5,6 +5,7 @@ import com.keklyndia.russpass.entity.Hotel;
 import com.keklyndia.russpass.entity.HotelPhoto;
 import com.keklyndia.russpass.service.HotelService;
 import com.keklyndia.russpass.util.SaveFIle;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,47 +17,48 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.nio.file.FileSystemException;
 
-@Controller
-@RequestMapping(path="/hotel")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/hotel")
 public class HotelController {
-    Logger logger = LoggerFactory.getLogger(HotelController.class);
-    @Autowired
-    private HotelService hotelService;
+    private final HotelService hotelService;
 
-    @PostMapping(path="/add")
-    public ResponseEntity<Hotel> addNewHotel (@RequestBody Hotel newHotel) {
+    @PostMapping
+    public ResponseEntity<Hotel> addNewHotel(@RequestBody Hotel newHotel) {
         hotelService.saveHotel(newHotel);
-        logger.info("Добавлен отель");
         return ResponseEntity.status(HttpStatus.CREATED).body(newHotel);
     }
 
-    @DeleteMapping(path="/{id}/delete")
-    public ResponseEntity<String> deleteHotel (@PathVariable(name = "id") Long id) {
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<String> deleteHotel(@PathVariable(name = "id") Long id) {
         hotelService.deleteHotel(id);
-        logger.info("Отель удален");
         return ResponseEntity.ok().body("успешно");
     }
-    @GetMapping(path="/all")
+
+    @GetMapping
     public @ResponseBody Iterable<Hotel> getAllHotels() {
         return hotelService.getAllHotels();
     }
-    @GetMapping(path="/{id}/photos")
+
+    @GetMapping(path = "/{id}")
     public @ResponseBody Iterable<HotelPhoto> getAllPhotoHotel(@PathVariable(name = "id") Long id) {
         return hotelService.getAllPhotoByHotelId(id);
     }
-    @GetMapping(path="/{id}")
+
+    @GetMapping(path = "/{id}")
     public @ResponseBody Hotel getHotelById(@PathVariable(name = "id") Long id) {
         return hotelService.getHotelById(id);
     }
-    @PostMapping(path="/addPhoto")
-    public ResponseEntity<HotelPhoto> addHotelNewPhoto (@RequestBody HotelPhoto newHotelPhoto) {
+
+    @PostMapping(path = "/addPhoto")
+    public ResponseEntity<HotelPhoto> addHotelNewPhoto(@RequestBody HotelPhoto newHotelPhoto) {
         hotelService.addHotelNewPhoto(newHotelPhoto);
         return ResponseEntity.status(HttpStatus.CREATED).body(newHotelPhoto);
     }
 
-    @PostMapping(path="/{id}/addPhoto")
-    public ResponseEntity<HotelPhoto> addHotelNewPhoto (@RequestParam("img") MultipartFile multipartFile,
-                                                                @PathVariable(name = "id") Long id) throws FileSystemException {
+    @PostMapping(path = "/{id}/addPhoto")
+    public ResponseEntity<HotelPhoto> addHotelNewPhoto(@RequestParam("img") MultipartFile multipartFile,
+                                                       @PathVariable(name = "id") Long id) throws FileSystemException {
         String fileName = SaveFIle.saveFile(multipartFile);
         HotelPhoto newHotelPhoto = new HotelPhoto();
         newHotelPhoto.setPhotoUrl(fileName);
@@ -64,11 +66,11 @@ public class HotelController {
         hotelService.addHotelNewPhoto(newHotelPhoto);
         return ResponseEntity.status(HttpStatus.CREATED).body(newHotelPhoto);
     }
-    @DeleteMapping(path="/{id}/deletePhoto/{idPhoto}")
-    public ResponseEntity<String> deleteHotelPhoto (@PathVariable(name = "id") Long id,
-                                                    @PathVariable(name = "idPhoto") Long idPhoto) {
+
+    @DeleteMapping(path = "/{id}/deletePhoto/{idPhoto}")
+    public ResponseEntity<String> deleteHotelPhoto(@PathVariable(name = "id") Long id,
+                                                   @PathVariable(name = "idPhoto") Long idPhoto) {
         hotelService.deleteHotelPhoto(id, idPhoto);
-        logger.info("Фото удалено");
         return ResponseEntity.ok().body("успешно");
     }
 }
---
+++ src/main/java/com/keklyndia/russpass/controller/HotelPhotoController.java
@@ -0,0 +1,45 @@
+package com.keklyndia.russpass.controller;
+
+import com.keklyndia.russpass.entity.HotelPhoto;
+import com.keklyndia.russpass.service.HotelService;
+import com.keklyndia.russpass.util.SaveFIle;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.FileSystemException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/hotel-photo")
+public class HotelPhotoController {
+    private final HotelService hotelService;
+
+    /**
+     * Прикрепить фото
+     * @param Файл фото
+     * @param id
+     * @return
+     * @throws FileSystemException
+     */
+    @PostMapping(path = "/{id}")
+    public ResponseEntity<HotelPhoto> addHotelNewPhoto(@RequestParam("img") MultipartFile multipartFile,
+                                                       @PathVariable(name = "id") Long id) throws FileSystemException {
+        String fileName = SaveFIle.saveFile(multipartFile);
+        HotelPhoto newHotelPhoto = new HotelPhoto();
+        newHotelPhoto.setPhotoUrl(fileName);
+        newHotelPhoto.setHotelId(id);
+        hotelService.addHotelNewPhoto(newHotelPhoto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(newHotelPhoto);
+    }
+
+    @DeleteMapping(path = "/{id}/deletePhoto/{idPhoto}")
+    public ResponseEntity<String> deleteHotelPhoto(@PathVariable(name = "id") Long id,
+                                                   @PathVariable(name = "idPhoto") Long idPhoto) {
+        hotelService.deleteHotelPhoto(id, idPhoto);
+        return ResponseEntity.ok().body("успешно");
+    }
+}
