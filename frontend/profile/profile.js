6ea5783686bbd7469152fafb3b80936d63077aa9
try2soc <anton.deryabin.2003@mail.ru>
2024-04-07 05:31:17 +0300

    Небольшие изменения

--- frontend/profile/profile.js
+++ frontend/profile/profile.js
@@ -6,4 +6,19 @@ document.addEventListener('DOMContentLoaded', () => {
 		const smallIconCat = document.querySelector('.close-cat')
 		smallIconCat.click()
 	}
+
+	const xp = document.getElementById("xp");
+	fetch("http://localhost:8181/level/xp/1")
+		.then(response => response.json())
+		.then(xpResponse => xp.textContent = xpResponse);
+
+	const level = document.getElementById("level");
+	fetch("http://localhost:8181/level/1")
+		.then(response => response.json())
+		.then(levelResponse => level.textContent = levelResponse);
+
+	for (let i = 1; i <= level; ++i) {
+		const present = document.getElementById("present-" + i);		
+		present.classList.add("available");
+	}
 })
